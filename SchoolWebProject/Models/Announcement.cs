c14624dedc1ba0dbdc27ffe9a50a95fe6b698104
HalynaVladyka <halyna.vladyka@gmail.com>
2015-10-25 11:06:36 +0200

    Some model classes

---
+++ SchoolWebProject/Models/Announcement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolWebProject.Models
+{
+    public class Announcement
+    {
+        public int Id { get; set; }
+        public string Topic { get; set; }
+        public string BriefDescription { get; set; }
+        public string Details { get; set; }
+
+     //   public string SchoolId { get; set; }
+        public School School { get; set; }
+
+     //   public string UserId { get; set; }
+        public User User { get; set; }
+
+        public DateTime DataPublished { get; set; }
+
+    }
+}
---
+++ SchoolWebProject/Models/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolWebProject.Models
+{
+    public class Subject
+    {
+        public int Id {get;set;}
+        public List<Teacher> Teachers { get; set; }
+        public string Name { get; set; }
+    }
+}

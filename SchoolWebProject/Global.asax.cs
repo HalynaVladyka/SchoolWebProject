0ceac498f3297aa8b0b17b3e4ff1ac592033d36a
HalynaVladyka <halyna.vladyka@gmail.com>
2015-10-25 14:42:48 +0200

    delete model folder

--- SchoolWebProject/Global.asax.cs
+++ SchoolWebProject/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
-using SchoolWebProject.Models;
+using SchoolWebProject.Model;
 
 namespace SchoolWebProject
 {
--- SchoolWebProject/Model/Degree.cs
+++ SchoolWebProject/Model/Degree.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace SchoolWebProject.Models
+namespace SchoolWebProject.Model
 {
     public class Degree
     {
